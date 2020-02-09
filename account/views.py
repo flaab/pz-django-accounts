8d363c7ab13aba54663cfc0179d2aa8cdbcdbddc
Arturo Lopez Perez <flaab.mrlinux@gmail.com>
2020-02-09 18:11:32 +0100

    import fix

--- account/views.py
+++ account/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required 
